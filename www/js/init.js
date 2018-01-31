d59a33a9fe220f4c092a7b9abfee717c30c846b1
pauli2406 <mpochert@web.de>
2018-02-01 00:37:23 +0100

    First attempts for nicknames

--- www/js/init.js
+++ www/js/init.js
@@ -1,4 +1,7 @@
 window.onload = function () {
+    MobileAccessibility.usePreferredTextZoom(false);
+    MobileAccessibility.setTextZoom(75);
+
     getSavedAddresses();
     addAddressOnClick();
     scanQRCode();
@@ -6,10 +9,18 @@ window.onload = function () {
 function addAddressOnClick() {
     $('#btnAddAddress').click(function () {
         var address = $('#inAddAddress').val();
-        var data = {address: address};
+        var nickname = $('#inNickname').val();
+        var saveArray = new Array();
+        saveArray.push({address: address,nickname: nickname});
+        window.localStorage.setArray(address,saveArray);
+
         var entries = document.getElementById("address_list").innerHTML;
+
+        var data = {address: address, nickname: nickname};
+
         document.getElementById("address_list").innerHTML = entries + tmpl("tmpl-transList", data);
-        window.localStorage.setItem(address, address);
+
+        // window.localStorage.setItem(address, address);
     })
 }
 
@@ -17,9 +28,14 @@ function scanQRCode() {
     $('#btnScanQr').click(function () {
         cordova.plugins.barcodeScanner.scan(
             function (result) {
-                window.localStorage.setItem(result.text, result.text);
+                var saveArray = new Array();
+                var nickname = $('#inNickname').val();
+                saveArray.push({address: result.text, nickname: nickname });
                 window.localStorage.setItem("selectedWallet", result.text);
+                window.localStorage.setArray(result.text,saveArray);
                 document.location.href = "wallet.html";
+                // window.localStorage.setItem(result.text, result.text);
+
             },
             function (error) {
                 alert("Scanning failed: " + error);
@@ -43,10 +59,18 @@ function scanQRCode() {
 function getSavedAddresses() {
     for (i = 0; i < window.localStorage.length; i++) {
         var savedWallets = window.localStorage;
+        debugger;
         for (i = 0; i < savedWallets.length; i++) {
+            debugger;
             var addr = savedWallets.key(i);
             if (!isEmpty(addr) && (addr.startsWith("G")) || addr.startsWith("g")) {
-                var data = {address: addr};
+
+                debugger;
+                var array = window.localStorage.getArray(addr);
+                array = array[0];
+                var data = {address: array.address, nickname: array.nickname};
+
+
                 var entries = document.getElementById("address_list").innerHTML;
                 document.getElementById("address_list").innerHTML = entries + tmpl("tmpl-transList", data);
                 var address = addr;
@@ -78,4 +102,11 @@ function deleteListEntry(address) {
 
 function isEmpty(str) {
     return (!str || 0 === str.length);
+}
+
+Storage.prototype.setArray = function(key, obj) {
+    return this.setItem(key, JSON.stringify(obj))
+}
+Storage.prototype.getArray = function(key) {
+    return JSON.parse(this.getItem(key))
 }
